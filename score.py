e3d153707b6552114da4587f442d0847623ded40
Cristian Consonni <kikkocristian@gmail.com>
2015-12-06 14:02:43 +0100

    Update license, fix type

--- score.py
+++ score.py
@@ -4,7 +4,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2013 Joan Creus <joan.creus.c@gmail.com>
-Copyright (c) 2015 Ricordisamoa and CristianCantoro <kikkocristian@gmail,com>
+Copyright (c) 2015 Ricordisamoa and CristianCantoro <kikkocristian@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
