cc311a2a6e3a52c38fa04aa6217db4cca09a2f77
Cristian Consonni <kikkocristian@gmail.com>
2015-12-09 00:20:45 +0100

    Produce HTML output in count_votes.sh

--- count_votes.sh
+++ count_votes.sh
@@ -53,7 +53,7 @@ rm -f books*_sublist.tsv*
 
 echo
 echo "Merging results..."
-python  merge.py -v results*_sublist.tsv
+python  merge.py -v  --html --html-output index.html results*_sublist.tsv
 
 echo
 echo "Done... removing temporary files"
